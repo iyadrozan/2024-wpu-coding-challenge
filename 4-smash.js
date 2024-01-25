2f3a9993fdde14d1abc684aed0afd6487c515162
iyadrozan <iyadrozan26@gmail.com>
2024-01-25 23:27:50 +0800

    Day 4/366 Sentence Smash

--- 4-smash.js
+++ 4-smash.js
@@ -3,18 +3,18 @@
 // https://www.codewars.com/kata/53dc23c68a0c93699800041d
 
 // Method 1
-function smash (words) {
-    let result = '';
+// function smash (words) {
+//     let result = '';
 
-    for(let i = 0; i < words.length; i++) {
-        result += words[i]
-        if(i != words.length - 1) {
-            result += ' ';
-        }
-    }
+//     for(let i = 0; i < words.length; i++) {
+//         result += words[i]
+//         if(i != words.length - 1) {
+//             result += ' ';
+//         }
+//     }
 
-    return result
-}
+//     return result
+// }
 
 // Method 2
 
@@ -24,7 +24,7 @@ function smash (words) {
 
 // One Line Method
 
-// const smash = (words) => words.join(' ')
+const smash = (words) => words.join(' ')
 
 console.log(
     smash(["this", "is", "a", "really", "long", "sentence"])
