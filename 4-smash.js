5d8bd7b482d123bf8ffdc297767c9af1c9146dff
iyadrozan <iyadrozan26@gmail.com>
2024-01-25 23:27:20 +0800

    Day 4/366 Sentence smash

---
+++ 4-smash.js
@@ -0,0 +1,31 @@
+// WPU Coding Challenge 2024
+// 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// Method 1
+function smash (words) {
+    let result = '';
+
+    for(let i = 0; i < words.length; i++) {
+        result += words[i]
+        if(i != words.length - 1) {
+            result += ' ';
+        }
+    }
+
+    return result
+}
+
+// Method 2
+
+// function smash (words) {
+//     return words.join(' ')
+// }
+
+// One Line Method
+
+// const smash = (words) => words.join(' ')
+
+console.log(
+    smash(["this", "is", "a", "really", "long", "sentence"])
+);
