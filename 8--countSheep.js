2b449d60d453f0f9e1fd27f83a4a541ca5e62745
iyadrozan <iyadrozan26@gmail.com>
2024-07-05 22:09:03 +0800

    Day 8/366

---
+++ 8--countSheep.js
@@ -0,0 +1,13 @@
+// WPU Coding Challenge 2024
+// 8/366
+// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+var countSheep = function (num){
+    let result = ''
+    for(let i = 1; i <= num; i++) {
+        result += i + ' sheep...'
+    }
+    return result;
+}
+
+console.log(countSheep(2))
